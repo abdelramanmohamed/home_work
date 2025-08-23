8ef7345e459e87ea493ce54b0a0427928a6b2b9c
abdelramanmohamed <bodxd4@gmail.com>
2025-08-23 23:48:27 +0300

    session4 done

--- session4/q20.dart
+++ session4/q20.dart
@@ -2,8 +2,17 @@
 Write a Dart program that checks access rules for a ticket gate. If the user is under 18, check if they
 have a parent. Use a switch statement on an area variable (general or restricted) to decide what
 message to print. */
-void main(){
-
-
-  
+void main() {
+  int age = 17;
+  bool haveParents = false;
+  String area = 'general';
+  switch (area) {
+    case 'general':
+      print('its general area');
+      break;
+    case 'restricted':
+      print('its restricted area');
+      break;
+      
+  }
 }
